bcb16d4697ec5e5bcf33de35f121b88a4e30bc2f
Justin Nelson <justin.john.nelson@gmail.com>
2011-06-14 21:46:40 -0500

    Working on getting widget to work

--- src/PriceWidget/src/CIAPI/Java/pricewidget/PriceWidget.java
+++ src/PriceWidget/src/CIAPI/Java/pricewidget/PriceWidget.java
@@ -1,17 +1,33 @@
 package CIAPI.Java.pricewidget;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 public class PriceWidget extends AppWidgetProvider {
+
+	private int updateCount = 0;
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		Log.i("CityIndex", "Updating City Index trading app");
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
 		ComponentName priceWidget = new ComponentName(context, PriceWidget.class);
-		remoteViews.setTextViewText(R.id.text, "Hello Widget, Again");
+		remoteViews.setTextViewText(R.id.text, "Hello Widget, " + updateCount++);
 		appWidgetManager.updateAppWidget(priceWidget, remoteViews);
+
+		final Intent intent = new Intent(context, UpdatePricesService.class);
+		final PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
+		final AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarm.cancel(pending);
+		long interval = 1000 * 60;
+		alarm.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), interval, pending);
 	}
 }
